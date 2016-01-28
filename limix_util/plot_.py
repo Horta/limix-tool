6af52eb93b8b5671378bf00448a35e76ef885d14
Danilo Horta <danilo.horta@gmail.com>
2016-01-28 08:47:17 +0000

    heatmap

--- limix_util/plot_.py
+++ limix_util/plot_.py
@@ -10,3 +10,18 @@ def show(fig=None):
         fout = os.path.join(folder, 'tmp.png')
         fig.savefig(fout)
         os.system("imgcat " + fout)
+
+def heatmap(X, ax=None):
+    ax = plt.gca() if ax is None else ax
+
+    im = ax.imshow(X)
+    plt.colorbar(im, ax=ax)
+
+    ax.set_frame_on(False)
+    ax.grid(False)
+
+if __name__ == '__main__':
+    import numpy as np
+    X = np.random.randn(1000,1000)
+    heatmap(X)
+    show()
