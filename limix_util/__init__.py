3cced96840627bda1996edb7ec940be0fb5004d4
Danilo Horta <danilo.horta@gmail.com>
2016-02-16 05:21:36 +0000

    cpypi working

--- limix_util/__init__.py
+++ limix_util/__init__.py
@@ -1,24 +1,31 @@
-import path_
-import pickle_
-import time_
-import str_
-import hdf5_
-import data_
-import plot_
-import inspect_
-import dist
-import h2
-import data_format
-from misc import traverse_dict
-from misc import ring
-from misc import BeginEnd
-from misc import group_by
-from progress import ProgressBar
-import cached
+# We first need to detect if we're being called as part of the limix-util setup
+# procedure itself in a reliable manner.
+try:
+    __LIMIX_UTIL_SETUP__
+except NameError:
+    __LIMIX_UTIL_SETUP__ = False
 
-def isfloat(value):
-    try:
-        float(value)
-        return True
-    except ValueError:
-        return False
+if not __LIMIX_UTIL_SETUP__:
+    import path_
+    import pickle_
+    import time_
+    import str_
+    import hdf5_
+    import data_
+    import plot_
+    import inspect_
+    import h2
+    import data_format
+    from misc import traverse_dict
+    from misc import ring
+    from misc import BeginEnd
+    from misc import group_by
+    from progress import ProgressBar
+    import cached
+
+    def isfloat(value):
+        try:
+            float(value)
+            return True
+        except ValueError:
+            return False
