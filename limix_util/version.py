1bf646de8bb2bb54b0234a7c00d25850ef5a014f
Danilo Horta <danilo.horta@gmail.com>
2016-02-16 04:23:59 +0000

    rm version

--- limix_util/version.py
+++
@@ -1,9 +0,0 @@
-
-# THIS FILE IS GENERATED FROM GWARPED SETUP.PY
-short_version = '0.1.1'
-version = '0.1.1'
-full_version = '0.1.1'
-git_revision = '25023b03a6325019d075560c28db1bc23e2912c1'
-release = True
-if not release:
-    version = full_version
