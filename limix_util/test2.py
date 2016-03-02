c41e61d52d9a8745b12a22ef519c67dc38dc9a5d
Danilo Horta <danilo.horta@gmail.com>
2016-03-02 09:36:00 +0000

    testing

--- limix_util/test2.py
+++ limix_util/test2.py
@@ -1,54 +1,65 @@
 import numpy as np
 import h5py
 import dask.array as da
-
+from limix_misc.report import BeginEnd
 from limix_misc import hdf5_
 
 import os
 
 n = 1000
-p = 5000
+p = 50000
 
-chroms = ['chrom%02d' % i for i in range(1, 23)]
-
-random = np.random.RandomState(53)
-with h5py.File('test-chrom-row.hdf5', 'w') as f:
-    for chrom in chroms:
-        f.create_dataset(chrom, data=G, chunks=(1, G.shape[1]), shuffle=True,
-                         compression="lzf")
+chroms = ['chrom%02d' % i for i in range(1, 2)]
 
 random = np.random.RandomState(53)
 with h5py.File('test-chrom-col.hdf5', 'w') as f:
     for chrom in chroms:
-        f.create_dataset(chrom, data=G, chunks=(G.shape[0], 1), shuffle=True,
-                         compression="lzf")
+        G = np.random.randn(n, p)
+        f.create_dataset(chrom, data=G, shuffle=True, compression="lzf")
 
-def conc(x):
-    return np.concatenate(x)
+def good_column_chunk(dataset, size=10*1024**2/8.):
+    shape = dataset.shape
+    if hasattr(dataset, 'chunks'):
+        chunks = dataset.chunks
+        n = size // (shape[0] * chunks[1])
+        n = max(n, 1)
+        return (shape[0], min(int(n*chunks[1]), shape[1]))
 
-from limix_misc.dask_ import hstack_row_read, hstack_col_read
-from limix_misc.report import BeginEnd
+    n = size // (shape[0] * 1)
+    n = max(n, 1)
+    return (shape[0], min(int(n), shape[1]))
 
-with h5py.File('test-chrom-row.hdf5', 'r') as f:
-    Xs = [f['chrom01'], f['chrom02']]
-    X = hstack_row_read(Xs)
-    with BeginEnd('Tipo row'):
-        X.dot(X.T).compute()
+def dot_(filepath, path):
+    with h5py.File(filepath, 'r') as f:
+        G = f[path]
+        print(good_column_chunk(G))
+        G = da.from_array(G, chunks=good_column_chunk(G))
+        G.dot(G.T).compute().shape
 
-with h5py.File('test-chrom-col.hdf5', 'r') as f:
-    Xs = [f['chrom01'], f['chrom02']]
-    X = hstack_col_read(Xs)
-    with BeginEnd('Tipo col'):
-        X.dot(X.T).compute()
-
-with h5py.File('test-chrom-row.hdf5', 'r') as f:
-    Xs = [f['chrom01'], f['chrom02']]
-    X = hstack_row_read(Xs)
-    with BeginEnd('Tipo row'):
-        X.std().compute()
-
-with h5py.File('test-chrom-col.hdf5', 'r') as f:
-    Xs = [f['chrom01'], f['chrom02']]
-    X = hstack_col_read(Xs)
-    with BeginEnd('Tipo col'):
-        X.std().compute()
+dot_('test-chrom-col.hdf5', 'chrom01')
+# from limix_misc.dask_ import hstack_row_read, hstack_col_read
+#
+#
+# with h5py.File('test-chrom-row.hdf5', 'r') as f:
+#     Xs = [f['chrom01'], f['chrom02']]
+#     X = hstack_row_read(Xs)
+#     with BeginEnd('Tipo row'):
+#         X.dot(X.T).compute()
+#
+# with h5py.File('test-chrom-col.hdf5', 'r') as f:
+#     Xs = [f['chrom01'], f['chrom02']]
+#     X = hstack_col_read(Xs)
+#     with BeginEnd('Tipo col'):
+#         X.dot(X.T).compute()
+#
+# with h5py.File('test-chrom-row.hdf5', 'r') as f:
+#     Xs = [f['chrom01'], f['chrom02']]
+#     X = hstack_row_read(Xs)
+#     with BeginEnd('Tipo row'):
+#         X.std().compute()
+#
+# with h5py.File('test-chrom-col.hdf5', 'r') as f:
+#     Xs = [f['chrom01'], f['chrom02']]
+#     X = hstack_col_read(Xs)
+#     with BeginEnd('Tipo col'):
+#         X.std().compute()
