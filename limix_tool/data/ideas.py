846ab82e1e0183697e8b82765b87b110b9f9054f
Danilo Horta <danilo.horta@gmail.com>
2016-03-09 10:04:40 +0000

    limix data

---
+++ limix_tool/data/ideas.py
@@ -0,0 +1,24 @@
+class MatrixVariate(object):
+    # +, -, dot, sum, std, mean
+    # transpose
+    # slicing
+    # __array__ protocol
+
+    def row_hdr(self, hdr_name):
+        pass
+
+    def col_hdr(self, hdr_name):
+        pass
+
+    def row_hdr_names(self):
+        pass
+
+    def col_hdr_names(self):
+        pass
+
+class GenotypeMatrix(MatrixVariate):
+    # def chrom(self, chromid):
+    #     # assert isint_like(chromid)
+    #     pass
+
+    pass
