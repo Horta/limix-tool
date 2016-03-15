62ee53f74dee7a07afe07de079f1d06029189733
Danilo Horta <danilo.horta@gmail.com>
2016-03-15 11:41:43 +0000

    hwe test

--- limix_tool/qtl/stats.py
+++ limix_tool/qtl/stats.py
@@ -1,6 +1,11 @@
+from __future__ import division
 import scipy.stats as st
+from scipy.misc import logsumexp
+import numpy as np
+from numpy import log
 from numpy import partition
 from numpy import mean
+from limix_util.scalar import isint
 
 def _get_median_terms(n):
     if n % 2 == 0:
@@ -30,7 +35,56 @@ def qvalues(pv):
     qvalue = importr('qvalue')
     print qvalue.qvalue(pv)
 
-if __name__ == '__main__':
-    import numpy as np
-    pv = np.random.rand(5)
-    qvalues(pv)
+def hwe_test(n_ab, n_a, n_b):
+    """ Exact test for Hardy-Weinberg Equilibrium for biallelic genotype.
+
+    This statistics is described in "A Note on Exact Tests of
+    Hardy-Weinberg Equilibrium."
+
+    :param int n_a: Number of the rarer alleles.
+    :param int n_b: Number of the common alleles.
+    :param int n_ab: Number of heterozygous AB genotypes.
+
+    Note that n_aa = (n_a - n_ab)/2 and n_bb = (n_b - n_ab)/2.
+    """
+    assert isint(n_a)
+    n_a = int(n_a)
+    assert isint(n_b)
+    n_b = int(n_b)
+    assert isint(n_ab)
+    n_ab = int(n_ab)
+    assert n_a <= n_b
+    assert n_a >= n_ab
+    assert (n_a + n_b) % 2 == 0
+    # N = n_ab + int((n_a - n_ab)/2) + int((n_b - n_ab)/2)
+
+    def ln_upper_weight(n_ab):
+        n_aa = int((n_a - n_ab)/2)
+        n_bb = int((n_b - n_ab)/2)
+        return log(4) + log(n_aa) + log(n_bb) - log(n_ab + 2) - log(n_ab + 1)
+
+    def ln_lower_weight(n_ab):
+        n_aa = int((n_a - n_ab)/2)
+        n_bb = int((n_b - n_ab)/2)
+        return log(n_ab) + log(n_ab - 1) - log(4) - log(n_aa + 1) - log(n_bb + 1)
+
+    lnP = np.empty(int(n_a / 2) + 1)
+    # this can be an arbitrary as long as I normalize the probabilities
+    # at the end
+    lnP[int(n_ab / 2)] = 0.
+    n_ab_i = n_ab
+    while n_ab_i >= 2:
+        i = int(n_ab_i / 2)
+        lnP[i-1] = lnP[i] + ln_lower_weight(n_ab_i)
+        n_ab_i -= 2
+
+    n_ab_i = n_ab
+    while n_ab_i <= n_a - 2:
+        i = int(n_ab_i / 2)
+        lnP[i+1] = lnP[i] + ln_upper_weight(n_ab_i)
+        n_ab_i += 2
+
+    lnPab = lnP[int(n_ab / 2)]
+    ok = lnPab >= lnP
+    lnC = logsumexp(lnP)
+    return np.exp(logsumexp(lnP[ok]) - lnC)
---
+++ limix_tool/qtl/test/test_stats.py
@@ -0,0 +1,41 @@
+import unittest
+import numpy as np
+from limix_tool.qtl.stats import hwe_test
+
+class TestStats(unittest.TestCase):
+    def test_hwe_test_odd(self):
+        N = 100
+        n_a = 21
+        n_b = 2*N - n_a
+        pv = []
+        n_abs = range(1, n_a+2, 2)
+        for n_ab in n_abs:
+            n_b = (N - (n_a-n_ab)/2 - n_ab) * 2 + n_ab
+            pv.append(hwe_test(n_ab, n_a, n_b))
+
+        pv_ideal = [2.2528461190398889e-13, 1.3389415434160398e-10,
+                    2.1307043119526107e-08, 1.4247843459546118e-06,
+                    4.8363303029661291e-05, 0.00091885946770931307,
+                    0.010293433548874778, 0.069576454405006927,
+                    0.28404150044336751, 1.0, 0.59364517592731658]
+        np.testing.assert_almost_equal(pv_ideal, pv)
+
+    def test_hwe_test_even(self):
+        N = 100
+        n_a = 20
+        n_b = 2*N - n_a
+        pv = []
+        n_abs = range(0, n_a+2, 2)
+        for n_ab in n_abs:
+            n_b = (N - (n_a-n_ab)/2 - n_ab) * 2 + n_ab
+            pv.append(hwe_test(n_ab, n_a, n_b))
+
+        pv_ideal = [1.0727838662094675e-14, 1.9320837430432614e-11,
+                    5.175120098433163e-09, 4.8913281073122185e-07,
+                    2.1541292353257525e-05, 0.00050433748452852699,
+                    0.0067221672322403288, 0.052638448446112096,
+                    0.24319101548367975, 1.0, 0.59149515040312262]
+        np.testing.assert_almost_equal(pv_ideal, pv)
+
+if __name__ == '__main__':
+    unittest.main()
