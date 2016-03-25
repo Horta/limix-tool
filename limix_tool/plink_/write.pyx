a327dcfeb780e7573baf9a57a0b70d6d11f6ac52
Danilo Horta <danilo.horta@gmail.com>
2016-03-25 21:48:12 +0000

    write pxd for python3

--- limix_tool/plink_/write.pyx
+++ limix_tool/plink_/write.pyx
@@ -1,7 +1,11 @@
 from libc.stdlib cimport malloc, free
 from libc.stdio cimport FILE, sprintf
 from libc.string cimport strlen
-from cpython.string cimport PyString_AsString
+# from cpython.string cimport PyString_AsString
+from cpython.unicode cimport PyUnicode_AsUTF8String
+from cpython.bytes cimport PyBytes_AsString
+from cpython.object cimport PyObject
+# from cpython.ref cimport Py_DECREF
 cimport cython
 
 cdef extern from "stdio.h":
@@ -10,14 +14,17 @@ cdef extern from "stdio.h":
     int fclose(FILE *)
     ssize_t getline(char **, size_t *, FILE *)
 
-@cython.boundscheck(False)
-@cython.wraparound(False)
-@cython.initializedcheck(False)
-cdef char ** to_cstring_array(list_str):
-    cdef char **ret = <char **>malloc(len(list_str) * sizeof(char *))
-    for i in xrange(len(list_str)):
-        ret[i] = PyString_AsString(list_str[i])
-    return ret
+# @cython.boundscheck(False)
+# @cython.wraparound(False)
+# @cython.initializedcheck(False)
+# cdef char ** to_cstring_array(list_str):
+#     cdef char **ret = <char **>malloc(len(list_str) * sizeof(char *))
+#     cdef PyObject *p
+#     for i in xrange(len(list_str)):
+#         p = PyUnicode_AsUTF8String(list_str[i])
+#         ret[i] = PyBytes_AsString(p)
+#         # ret[i] = PyString_AsString(list_str[i])
+#     return ret
 
 #  chromosome (1-22, X, Y or 0 if unplaced)
 #  rs# or snp identifier
@@ -29,17 +36,23 @@ cdef char ** to_cstring_array(list_str):
 cpdef write_map(dst_filepath, long[:] chroms, list rss,
                  long[:] gds, long[:] bps):
 
-    cdef char **c_rss = to_cstring_array(rss)
+    # cdef char **c_rss = to_cstring_array(rss)
+    cdef char *c
     cdef char str_[64]
     cdef long i
+    cdef bytes p
     f = fopen(dst_filepath, 'w')
     for i in range(chroms.shape[0]):
+
+        p = PyUnicode_AsUTF8String(rss[i])
+        c = PyBytes_AsString(p)
 
         sprintf(str_, "%d", chroms[i]);
         fwrite(str_, 1, strlen(str_), f)
         fwrite(' ', 1, 1, f)
 
-        fwrite(c_rss[i], 1, strlen(c_rss[i]), f)
+        fwrite(c, 1, strlen(c), f)
+        # fwrite(c_rss[i], 1, strlen(c_rss[i]), f)
         fwrite(' ', 1, 1, f)
 
         sprintf(str_, "%d", gds[i]);
@@ -51,7 +64,7 @@ cpdef write_map(dst_filepath, long[:] chroms, list rss,
         fwrite('\n', 1, 1, f)
 
     fclose(f)
-    free(c_rss)
+    # free(c_rss)
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
