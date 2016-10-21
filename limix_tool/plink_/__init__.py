c75f2e47fa0510e0d4fb7ecec2ea0674830086dc
Danilo Horta <danilo.horta@gmail.com>
2016-10-21 11:18:34 +0100

    no plink

--- limix_tool/plink_/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import absolute_import
-from .main import create_ped
-from .main import create_map
-from .main import create_phen
-from .main import create_bed
--- limix_tool/plink_/write.pxd
+++
@@ -1,3 +0,0 @@
-cpdef write_phen_int(dst_filepath, long[:] y)
-cpdef write_map(dst_filepath, long[:] chroms, list rss,
-                 long[:] gds, long[:] bps)
