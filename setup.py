7a24c03250b7bb455898f3ce9c2fb3f5f4394148
Danilo Horta <danilo.horta@gmail.com>
2016-10-24 23:13:55 +0100

    veersion bump

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def setup_package():
 
     metadata = dict(
         name='limix_tool',
-        version='1.0.0',
+        version='1.0.1',
         maintainer="Limix Developers",
         maintainer_email="horta@ebi.ac.uk",
         license="MIT",
